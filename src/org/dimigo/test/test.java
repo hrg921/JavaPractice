3e909bca966728f4a8c6c09d0d297353d4633312
HD132635 <hrg921@gmail.com>
2015-10-13 21:05:19 +0900

    test

--- src/org/dimigo/test/test.java
+++ src/org/dimigo/test/test.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2015. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package org.dimigo.test;
 
 /**
@@ -6,7 +14,7 @@ package org.dimigo.test;
 public class test {
 
     public static void main(String[] args) {
-
+        System.out.println("GitHub Upload Test");
     }
 
 }
