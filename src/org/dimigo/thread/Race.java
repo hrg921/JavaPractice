017818650b1fbd5eea6a04f2f9162f7c0dddd2bd
HD132635 <hrg921@gmail.com>
2015-11-05 11:24:11 +0900

    homework submit

---
+++ src/org/dimigo/thread/Race.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2015. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package org.dimigo.thread;
+
+/**
+ * Created by hangeonho on 15. 11. 5..
+ */
+public class Race {
+
+    public static void main(String[] args) {
+        Runner runner1 = new Runner("홍길동");
+        Runner runner2 = new Runner("홍길순");
+
+        System.out.println("main thread start");
+
+        runner1.start();
+        runner2.start();
+
+        System.out.println("main thread stop");
+
+    }
+
+}
