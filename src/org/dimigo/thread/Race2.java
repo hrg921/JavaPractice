aa42260140fe7918cd47375f4800f8783e245b16
HD132635 <hrg921@gmail.com>
2015-11-05 11:46:44 +0900

    homework submit

---
+++ src/org/dimigo/thread/Race2.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2015. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package org.dimigo.thread;
+
+/**
+ * Created by hangeonho on 15. 11. 5..
+ */
+public class Race2 {
+
+    public static void main(String[] args) {
+        Thread runner1 = new Thread(new Runner2("홍길동"));
+        Thread runner2 = new Thread(new Runner2("홍길순"));
+
+        System.out.println("main thread start");
+
+        runner1.setPriority(Thread.MAX_PRIORITY);
+        runner2.setPriority(Thread.MAX_PRIORITY);
+
+        runner1.start();
+        runner2.start();
+
+        System.out.println("main thread stop");
+    }
+
+}
---
+++ src/org/dimigo/thread/Runner2.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2015. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package org.dimigo.thread;
+
+/**
+ * Created by hangeonho on 15. 11. 5..
+ */
+public class Runner2 implements Runnable{
+
+    private String name;
+
+    public Runner2() {}
+
+    public Runner2(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(name + " 출발");
+        for(int i = 0; i < 11; i ++) {
+            System.out.println(name + " 출발 " + (10 - i) * 10 + " 미터");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(name + " 골인");
+    }
+
+}
